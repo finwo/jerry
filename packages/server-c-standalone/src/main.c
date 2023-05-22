a5dcd4617ef99cf1379918cc51022487a2a5819b
Yersa Nordman <yersa@finwo.nl>
2023-05-22 22:28:54 +0200

    Dynamic listen address, add 404 support

--- packages/server-c-standalone/src/main.c
+++ packages/server-c-standalone/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "finwo/http-parser.h"
 #include "finwo/http-server.h"
@@ -18,16 +19,43 @@ void route_get_hello(struct hs_udata *reqdata) {
   return;
 }
 
-int main() {
-  const char *addrs[] = { "tcp://localhost:4000" };
+void route_404(struct hs_udata *reqdata) {
+  http_parser_header_set(reqdata->reqres->response, "Content-Type", "text/plain");
+  reqdata->reqres->response->status   = 404;
+  reqdata->reqres->response->body     = strdup("Not found");
+  reqdata->reqres->response->bodysize = strlen(reqdata->reqres->response->body);
+  http_server_response_send(reqdata, true);
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  char *addr_intermediate = "localhost:4000";
+  int i;
+
+  for(i=0; i<argc; i++) {
+
+    if (!strcmp("--listen", argv[i])) {
+      i++;
+      addr_intermediate = argv[i];
+      continue;
+    }
+
+  }
+
+  char *addr;
+  asprintf(&addr, "tcp://%s", addr_intermediate);
+  const char *addrs[] = { addr };
 
   struct http_server_events evs = {
-    .tick    = NULL,
-    .serving = onServing,
-    .error   = NULL,
-    .close   = NULL,
+    .tick     = NULL,
+    .serving  = onServing,
+    .error    = NULL,
+    .close    = NULL,
+    .notFound = route_404
   };
 
   http_server_route("GET", "/hello", route_get_hello);
   http_server_main(addrs, sizeof(addrs) / sizeof(void*), &evs, NULL);
+
+  return 42;
 }
