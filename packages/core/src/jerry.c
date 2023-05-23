f392fabbbd6ceceaa350b120231530d48f9db02a
Yersa Nordman <yersa@finwo.nl>
2023-05-24 00:01:23 +0200

    Re-enable post route

--- packages/core/src/jerry.c
+++ packages/core/src/jerry.c
@@ -16,76 +16,75 @@ struct llistener {
 
 struct llistener *listeners = NULL;
 
-void jerry_route_post(struct http_parser_event *ev) {
-  struct hs_udata *hsdata = ev->udata;
-  struct evio_conn *conn  = hsdata->connection;
+void _jerry_respond_error(
+  struct hs_udata *hsdata,
+  int status,
+  const char *message
+) {
+  response->status = status;
+  http_parser_header_set(response, "Content-Type", "application/json");
+  asprintf(&(response->body), "{\"ok\":false,\"message\":\"%s\"}", message);
+  response->bodysize = strlen(response->body);
+  char *buffer = http_parser_sprint_response(response);
+  evio_conn_write(hsdata->connection, buffer, strlen(buffer));
+  free(buffer);
+  evio_conn_close(hsdata->connection);
+}
 
-  /* struct conn_udata *conndata = ev->udata; */
-  /* struct llist_conns *listener; */
-  /* struct llist_conns *prev_listener = NULL; */
-  /* char *response_buffer; */
-  /* char *event_buffer; */
-  /* char *event_tmp; */
-  /* int listener_count = 0; */
+void jerry_route_post(struct hs_udata *hsdata) {
+  struct evio_conn *conn = hsdata->connection;
 
-  /* // Fetching the request */
-  /* // Has been wrapped in http_parser_event to support more features in the future */
-  /* struct http_parser_message *request  = ev->request; */
-  /* struct http_parser_message *response = ev->response; */
-  /* struct http_parser_header *header    = NULL; */
+  struct llist_conns *listener;
+  struct llist_conns *prev_listener = NULL;
+  char *chunk_json;
+  char *chunk;
 
-  /* // Parse and validate the body */
-  /* JSON_Value *jEvent = json_parse_string(request->body); */
-  /* if (json_value_get_type(jEvent) != JSONObject) { */
-  /*   json_value_free(jEvent); */
+  // Create easy references
+  struct http_parser_message *request  = hsdata->reqres->request;
+  struct http_parser_message *response = hsdata->reqres->response;
 
-  /*   // Send error in return */
-  /*   response->status = 422; */
-  /*   http_parser_header_set(response, "Content-Type", "application/json"); */
-  /*   response->body     = strdup("{\"ok\":false,\"message\":\"Only JSON objects are allowed\"}"); */
-  /*   response->bodysize = strlen(response->body); */
-  /*   response_buffer    = http_parser_sprint_response(response); */
-  /*   evio_conn_write(conndata->connection, response_buffer, strlen(response_buffer)); */
-  /*   free(response_buffer); */
-  /*   evio_conn_close(conndata->connection); */
-  /*   return; */
-  /* } */
+  // Parse and validate the body
+  JSON_Value *jEvent = json_parse_string(request->body);
+  if (json_value_get_type(jEvent) != JSONObject) {
+    json_value_free(jEvent);
+    _jerry_respond_error(hsdata, 422, "Only JSON objects are allowed");
+    return;
+  }
 
-  /* // Here = valid json object, we need to propagate the event to all listeners */
+  // TODO: check if the required fields are set
 
-  /* // Pre-render json to distribute */
-  /* event_tmp = json_serialize_to_string(jEvent); */
-  /* asprintf(&event_buffer, "%x\r\n%s\n\r\n", strlen(event_tmp) + 1, event_tmp); */
-  /* json_value_free(jEvent); */
-  /* free(event_tmp); */
+  // Here = valid json object, we need to propagate the event to all listeners
 
-  /* // Dsitribute */
-  /* listener = listeners; */
-  /* while(listener) { */
-  /*   listener_count++; */
-  /*   evio_conn_write(listener->data, event_buffer, strlen(event_buffer)); */
-  /*   prev_listener = listener; */
-  /*   listener      = listener->next; */
-  /* } */
+  // Pre-render json into chunk to distribute
+  chunk_json = json_serialize_to_string(jEvent);
+  asprintf(&chunk, "%x\r\n%s\n\r\n", strlen(chunk_json) + 1, chunk_json);
+  json_value_free(jEvent);
+  json_free_serialized_string(chunk_json);
 
-  /* printf("Listener count: %d\n", listener_count); */
+  // Dsitribute
+  listener = listeners;
+  while(listener) {
+    evio_conn_write(listener->conn, event_buffer, strlen(event_buffer));
+    prev_listener = listener;
+    listener      = listener->next;
+  }
 
-  /* // Clean up event */
-  /* free(event_buffer); */
+  // Clean up event
+  free(chunk);
 
-  /* // Build response */
-  /* response->status = 200; */
-  /* http_parser_header_set(response, "Content-Type", "application/json"); */
-  /* response->body     = strdup("{\"ok\":true}"); */
-  /* response->bodysize = strlen(response->body); */
+  // Build response
+  response->status = 200;
+  http_parser_header_set(response, "Content-Type", "application/json");
+  response->body     = strdup("{\"ok\":true}");
+  response->bodysize = strlen(response->body);
 
-  /* // Send response */
-  /* response_buffer = http_parser_sprint_response(response); */
-  /* evio_conn_write(conndata->connection, response_buffer, strlen(response_buffer)); */
-  /* free(response_buffer); */
+  // Send response
+  response_buffer = http_parser_sprint_response(response);
+  evio_conn_write(hsdata->connection, response_buffer, strlen(response_buffer));
+  free(response_buffer);
 
-  /* // Aanndd.. we're done */
-  /* evio_conn_close(conndata->connection); */
+  // Aanndd.. we're done
+  evio_conn_close(hsdata->connection);
 }
 
 void jerry_route_get(struct hs_udata *hsdata) {
@@ -95,7 +94,6 @@ void jerry_route_get(struct hs_udata *hsdata) {
   // Has been wrapped in http_parser_event to support more features in the future
   struct http_parser_message *request  = hsdata->reqres->request;
   struct http_parser_message *response = hsdata->reqres->response;
-  struct http_parser_header *header    = NULL;
 
   // Build response
   response->status = 200;
